99ad65d511813f85e6346c373374e0f1cbca3c6a
james <phantombm@smartlink.io>
2017-07-17 14:49:30 +0900

    update inspinia version to 2.7

--- imports/ui/pages/DataTable/DataTable.jsx
+++ imports/ui/pages/DataTable/DataTable.jsx
@@ -1,29 +1,6 @@
 import React, { Component } from 'react';
 
 export default class DataTable extends Component {
-  componentDidMount() {
-    $('.dataTables-example').DataTable({
-      dom: '<"html5buttons"B>lTfgitp',
-      buttons: [
-        { extend: 'copy'},
-        {extend: 'csv'},
-        {extend: 'excel', title: 'ExampleFile'},
-        {extend: 'pdf', title: 'ExampleFile'},
-
-        {extend: 'print',
-          customize: function (win){
-            $(win.document.body).addClass('white-bg');
-            $(win.document.body).css('font-size', '10px');
-
-            $(win.document.body).find('table')
-              .addClass('compact')
-              .css('font-size', 'inherit');
-          }
-        }
-      ]
-    });
-  }
-
   render() {
     return (
       <div className="wrapper wrapper-content animated fadeInRight">
